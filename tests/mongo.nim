3d089a08efe671ee2da94f8354c8204cee3598c4
EXetoC <exetoc@gmail.com>
2014-02-18 00:17:12 +0100

    Use the unittest module.

--- tests/mongo.nim
+++ tests/mongo.nim
@@ -1,125 +1,149 @@
-import db_mongo, sequtils, times, oids
+import db_mongo, sequtils, times, oids, unittest
 
-var
-  arr2 = ["o", "p", "q"]
-  bson = %%{
-    "int32": %1i32,
-    "int64": %2i64,
-    "obj": %{
-      "field1": %6i32,
-      "field2": %7i64,
-      "field3": %"field3val"
-    },
-    "arr1": %[
-      %12i32,
-      %13i64,
-      %"arr1str"
-    ],
-    "arr2": %arr2
-  }
+suite "MongoDB":
+  test "BSON construction":
+    var
+      arr2 = ["o", "p", "q"]
+      bson = %%{
+        "int32": %1i32,
+        "int64": %2i64,
+        "obj": %{
+          "field1": %6i32,
+          "field2": %7i64,
+          "field3": %"field3val"
+        },
+        "arr1": %[
+          %12i32,
+          %13i64,
+          %"arr1str"
+        ],
+        "arr2": %arr2
+      }
 
-assert bson["int32"].int32Val == 1
-assert bson["int64"].int64Val == 2
+    test "BSON iteration":
+      for x in bson:
+        case x.k:
+        of "int32":
+          check:
+            x.v.kind == bkInt32
+            x.v.int32Val == 1
+        of "int64":
+          check:
+            x.v.kind == bkInt64
+            x.v.int64Val == 2
+        of "obj":
+          for y in bson.subItems:
+            case y.k:
+            of "field1":
+              check:
+                y.v.kind == bkInt32
+                y.v.int32Val == 6
+            of "field2":
+              check:
+                y.v.kind == bkInt64
+                y.v.int64Val == 7
+            of "field3":
+              check:
+                y.v.kind == bkStr
+                y.v.strVal == "field3val"
+            else:
+              check false
+        of "arr1":
+          for y in bson.subItems:
+            case y.k:
+            of "0":
+              check:
+                y.v.kind == bkInt32
+                y.v.int32Val == 12
+            of "1":
+              check:
+                y.v.kind == bkInt64
+                y.v.int64Val == 13
+            of "2":
+              check:
+                y.v.kind == bkStr
+                y.v.strVal == "arr1str"
+            else:
+              check false
+        of "arr2":
+          for y in bson.subItems:
+            case y.k:
+            of "0":
+              check:
+                y.v.kind == bkStr
+                y.v.strVal == "o"
+            of "1":
+              check:
+                y.v.kind == bkStr
+                y.v.strVal == "p"
+            of "2":
+              check:
+                y.v.kind == bkStr
+                y.v.strVal == "q"
+            else:
+              check false
+        else:
+          check false
 
-assert bson["obj"]["field1"].int32Val == 6
-assert bson["obj"]["field2"].int64Val == 7
-assert bson["obj"]["field3"].strVal == "field3val"
+    test "BSON access":
+      check:
+        bson["int32"].int32Val == 1
+        bson["int64"].int64Val == 2
+        
+        bson["obj"]["field1"].int32Val == 6
+        bson["obj"]["field2"].int64Val == 7
+        bson["obj"]["field3"].strVal == "field3val"
+        
+        bson["arr1"]["0"].int32Val == 12
+        bson["arr1"]["1"].int64Val == 13
+        bson["arr1"]["2"].strVal == "arr1str"
+        
+        bson["arr2"]["0"].strVal == "o"
+        bson["arr2"]["1"].strVal == "p"
+        bson["arr2"]["2"].strVal == "q"
 
-assert bson["arr1"]["0"].int32Val == 12
-assert bson["arr1"]["1"].int64Val == 13
-assert bson["arr1"]["2"].strVal == "arr1str"
+        bson["arr2"][string] == @["o", "p", "q"]
 
-assert bson["arr2"]["0"].strVal == "o"
-assert bson["arr2"]["1"].strVal == "p"
-assert bson["arr2"]["2"].strVal == "q"
+    test "Database access":
+      var
+        db = db_mongo.open()
+      db.insert("test.nim_mongo_test", bson)
+      for x in db.find("test.nim_mongo_test", limit = 1):
+        echo "DB search result:"
+        print x
 
-# TODO: Assert that it fails when not all elements are of the specified type.
-assert bson["arr2"][string] == @["o", "p", "q"]
+      test "Find result":
+        var
+          findResult: seq[PBson]
 
-for x in bson:
-  case x.k:
-  of "int32":
-    assert x.v.kind == bkInt32
-    assert x.v.int32Val == 1
-  of "int64":
-    assert x.v.kind == bkInt64
-    assert x.v.int64Val == 2
-  of "obj":
-    for y in bson.subItems:
-      case y.k:
-      of "field1":
-        assert y.v.kind == bkInt32
-        assert y.v.int32Val == 6
-      of "field2":
-        assert y.v.kind == bkInt64
-        assert y.v.int64Val == 7
-      of "field3":
-        assert y.v.kind == bkStr
-        assert y.v.strVal == "field3val"
-      else:
-        assert false
-  of "arr1":
-    for y in bson.subItems:
-      case y.k:
-      of "0":
-        assert y.v.kind == bkInt32
-        assert y.v.int32Val == 12
-      of "1":
-        assert y.v.kind == bkInt64
-        assert y.v.int64Val == 13
-      of "2":
-        assert y.v.kind == bkStr
-        assert y.v.strVal == "arr1str"
-      else:
-        assert false
-  of "arr2":
-    for y in bson.subItems:
-      case y.k:
-      of "0":
-        assert y.v.kind == bkStr
-        assert y.v.strVal == "o"
-      of "1":
-        assert y.v.kind == bkStr
-        assert y.v.strVal == "p"
-      of "2":
-        assert y.v.kind == bkStr
-        assert y.v.strVal == "q"
-      else:
-        assert false
-  else:
-    assert false
+        findResult = toSeq(db.find("test.nim_mongo_test",
+          %%{"int32": %2i32},
+          %%{"int32": %1i32}))
+        check findResult.len == 0
 
-var
-  db = db_mongo.open()
+        findResult = toSeq(db.find("test.nim_mongo_test", limit = 1))
+        check findResult.len <= 1
 
-db.insert("test.nim_mongo_test", bson)
-for x in db.find("test.nim_mongo_test"):
-  print x
+        for x in db.find("test.nim_mongo_test",
+            %%{"int32": %1i32},
+            %%{"int32": %1i32}):
+          for y in x:
+            case y.k:
+            of "_id":
+              discard
+            of "int32":
+              check y.v.int32Val == 1
+            else:
+              check false
 
-assert toSeq(db.find("test.nim_mongo_test", limit = 1)).len <= 1
-
-for x in db.find("test.nim_mongo_test",
-    %%{"int32": %1i32}, %%{"int32": %1i32}):
-  for y in x:
-    case y.k:
-    of "_id":
-      discard
-    of "int32":
-      assert y.v.int32Val == 1
-    else:
-      assert false
-
-for x in db.find("test.nim_mongo_test", %%{"int32": %1i32}):
-  for y in x:
-    case y.k:
-    of "_id", "obj", "arr1", "arr2":
-      discard
-    of "int32":
-      assert y.v.int32Val == 1
-    of "int64":
-      assert y.v.int64Val == 2
-    else:
-      assert false
-
-assert toSeq(db.find("test.nim_mongo_test",
-  %%{"int32": %2i32}, %%{"int32": %1i32})).len == 0
+        for x in db.find("test.nim_mongo_test", %%{"int32": %1i32}):
+          for y in x:
+            case y.k:
+            of "_id", "obj", "arr1", "arr2":
+              discard
+            of "int32":
+              check y.v.int32Val == 1
+            of "int64":
+              check y.v.int64Val == 2
+            else:
+              check false
